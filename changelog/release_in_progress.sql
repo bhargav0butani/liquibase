360255d31b0b2e827ef819c1c5795bee8a268408
bhargav0butani <bhargav@iterasolutions.co.in>
2023-09-22 19:39:57 +0530

    Test liquibase action in github

--- changelog/release_in_progress.sql
+++ changelog/release_in_progress.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
---changeset ushja:membership_history_2.0 labels:membership_history2.0 context:development
+--changeset ushja:membership_history_3.0 labels:membership_history3.0 context:development
 --comment: Retrieve records from the membership_history table.
 SELECT * FROM membership_history;
