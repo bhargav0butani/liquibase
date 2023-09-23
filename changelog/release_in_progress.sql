21b9d4177cd0e0a112028141dbe5be126d94c914
bhargav0butani <bhargav@iterasolutions.co.in>
2023-09-23 12:31:11 +0530

    Added rollback changes

--- changelog/release_in_progress.sql
+++ changelog/release_in_progress.sql
@@ -1,5 +1,9 @@
 --liquibase formatted sql
 
---changeset ushja:membership_history_3.0 labels:membership_history3.0 context:development
---comment: Retrieve records from the membership_history table.
-SELECT * FROM membership_history;
+--changeset ushja:individual2.0 labels:Update_Individual.3874 context:development
+--comment: Update first_name in the individual table for individual id 3874.
+UPDATE individual
+SET first_name = 'BHARGAV 001'
+WHERE id = '3874'
+
+--rollback UPDATE individual SET first_name = 'Bhargav Itera' WHERE id = '3874'
